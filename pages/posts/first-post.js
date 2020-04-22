c4916cf4cfa921909da433b22f23b4d961bf8228
mlaskowski <maciejlaskowsky@gmail.com>
2020-04-22 23:59:00 +0200

    add meta tag title to new page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Link from 'next/link';
+import Head from 'next/head';
 
 const FirstPost = () => (
   <>
+    <Head>
+      <title>First Post</title>
+    </Head>
     <h1>First Post</h1>
     <h2>
       <Link href="/">
