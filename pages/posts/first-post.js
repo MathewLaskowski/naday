f882f47eaaca2cda433c9c62f7b60390801efefe
mlaskowski <maciejlaskowsky@gmail.com>
2020-04-22 23:52:04 +0200

    add link component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import Link from 'next/link';
 
-const FirstPost = () => <h1>First Post</h1>
+const FirstPost = () => (
+  <>
+    <h1>First Post</h1>
+    <h2>
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
+    </h2>
+  </>
+)
 
 export default FirstPost
